2b497ab81b69fa37b6e94cdf3af41b875cdbe5e6
Edjucatur <babzadelod@gmail.com>
2024-09-19 14:34:05 +0100

    djbcxjh;j

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let menu = document.querySelector("#menu-icon");
+let cancel = document.querySelector("#cancel");
+let navbar = document.querySelector(".navbar");
 
+menu.addEventListener("click", function () {
+  menu.classList.toggle("hidden");
+  cancel.classList.toggle("hidden");
+  navbar.classList.toggle("hidden");
+});
+
+cancel.addEventListener("click", function () {
+  cancel.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
+  navbar.classList.toggle("hidden");
+});
